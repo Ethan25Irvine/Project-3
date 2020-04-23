3636de5d53786ed079b12ab64a4f54e519187202
Jordan Albers <jordanalbers@Jordans-MacBook-Air.local>
2020-04-23 02:33:04 -0700

    added pages folder and components folder

---
+++ the-street/client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Contact from './pages/Contact.js'
+import './App.css';
+
+function App() {
+	return <div className="App" >
+		<Contact/>
+		</div>
+
+}
+
+export default App;
